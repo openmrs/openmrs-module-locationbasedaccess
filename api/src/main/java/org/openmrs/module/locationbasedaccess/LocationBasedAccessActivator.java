6dbd5a271d7bcfae8d14515ea37c8a79606c71ff
K.Suthagar <suthagar.14@cse.mrt.ac.lk>
2018-05-24 21:28:57 +0530

    Created new module for Location based access control project (#5)
    Minor modification
    
    Minor modifications
    
    Added new OpenMRS license header

---
+++ api/src/main/java/org/openmrs/module/locationbasedaccess/LocationBasedAccessActivator.java
@@ -0,0 +1,65 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+
+package org.openmrs.module.locationbasedaccess;
+
+import org.openmrs.module.locationbasedaccess.common.Helper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.openmrs.module.BaseModuleActivator;
+import org.openmrs.module.ModuleActivator;
+
+public class LocationBasedAccessActivator extends BaseModuleActivator {
+
+
+    protected Logger log = LoggerFactory.getLogger(getClass());
+
+    /**
+     * @see ModuleActivator#willRefreshContext()
+     */
+    public void willRefreshContext() {
+        log.info("Refreshing Location Based Access Control Module");
+    }
+
+    /**
+     * @see ModuleActivator#contextRefreshed()
+     */
+    public void contextRefreshed() {
+        log.info("Location Based Access Control Module refreshed");
+    }
+
+    /**
+     * @see ModuleActivator#willStart()
+     */
+    public void willStart() {
+        log.info("Starting Location Based Access Control Module");
+    }
+
+    /**
+     * @see ModuleActivator#started()
+     */
+    public void started() {
+        log.info("Location Based Access Control Module started");
+    }
+
+    /**
+     * @see ModuleActivator#willStop()
+     */
+    public void willStop() {
+        log.info("Stopping Location Based Access Control Module");
+    }
+
+    /**
+     * @see ModuleActivator#stopped()
+     */
+    public void stopped() {
+        log.info("Location Based Access Control Module stopped");
+    }
+}
