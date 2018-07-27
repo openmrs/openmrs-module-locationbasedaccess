be66611d3d915d765c206c74d097a21fa2d15536
Suthagar23 <suthagar23@gmail.com>
2018-07-27 15:05:19 +0530

    LBAC-13 Added implementation to create RefApp location glopal property
    LBAC-13 Added implementation to create RefApp location glopal property

--- api/src/main/java/org/openmrs/module/locationbasedaccess/LocationBasedAccessConstants.java
+++ api/src/main/java/org/openmrs/module/locationbasedaccess/LocationBasedAccessConstants.java
@@ -14,4 +14,6 @@ public class LocationBasedAccessConstants {
 
     public static final  String LOCATION_ATTRIBUTE_GLOBAL_PROPERTY_NAME = "locationbasedaccess.locationAttributeUuid";
     public static final  String LOCATION_USER_PROPERTY_NAME = "locationUuid";
+
+    public static final String REF_APP_LOCATION_USER_PROPERTY_NAME = "referenceapplication.locationUserPropertyName";
 }
